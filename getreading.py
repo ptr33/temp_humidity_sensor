b3b333d436fff99eed9859fbae1ddb19dac8ee03
ptr33 <94262565+ptr33@users.noreply.github.com>
2025-10-06 17:12:51 +0200

    Added heartbeat

--- getreading.py
+++ getreading.py
@@ -82,7 +82,8 @@ print(f'Selected {temp} C and {humidity} % dewpoint {dewpoint} C')
 # publish.single(config['topic_temperature'], 25.3, **config['server'])
 msgs  = [[config['topic_temperature'], temp],
          [config['topic_humidity'], humidity],
-         [config['topic_dewpoint'], dewpoint]]
+         [config['topic_dewpoint'], dewpoint],
+         [config['topic_heartbeat'], config['text_heartbeat']]]
 #         [config['topic_dewpoint'], f'{dewpoint}:0.1f']]
 publish.multiple(msgs, **config['server'])
 
